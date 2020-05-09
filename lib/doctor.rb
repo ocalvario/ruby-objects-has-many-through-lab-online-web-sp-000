dd028e1b777878e1bda618afe29c7d51b46ec097
Oswald Calvario <oswald.calvario@gmail.com>
2020-05-09 23:08:30 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -21,6 +21,9 @@ class Doctor
     Appointment.all.select {|appointment| appointment.doctor == self}
   end
   
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
   
   
 end 
